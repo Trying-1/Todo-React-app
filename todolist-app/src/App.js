b5768a1c1bce743f09f01efb576bef37dc127ca6
Trying-1 bait <tryingbait@gmail.com>
2023-10-13 13:46:02 +0000

    edited

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import TodoForm from "./component/TodoForm"
 function App() {
   return (
     <div className="App">
-      
+      <TodoForm/>
     </div>
   );
 }
---
+++ todolist-app/src/component/TodoForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TodoForm() {
+  return (
+    <div>
+        <input className='form-input' placeholder='what to add?'/>
+        <button className="form-btn">Add Task</button>
+    </div>
+  )
+}
+
+export default TodoForm
